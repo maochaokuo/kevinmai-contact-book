64e0aaa4ac324f3ffcebeb8e10227c5f9d9e338a
Thomas Mao <tm1813@gmail.com>
2021-07-11 09:51:50 +0800

    default rest api get "ok"

--- src/clj/contacts/core.clj
+++ src/clj/contacts/core.clj
@@ -1,4 +1,14 @@
-(ns contacts.core)
+(ns contacts.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/api" {:get (fn [req]
+                     {:status 200
+                      :body "ok"})}]])))
 
 (defn -main []
-  (println "hello world"))
+  (println "Server started")
+  (run-server app {:port 4000}))
